f032d6b9ef83c9ab8014477566cb30a09c249d3f
oguzhanozsy <oguzhanozsy37@gmail.com>
2023-10-20 07:30:56 +0300

    Initial commit

---
+++ 1)Objects and Data Structures/1.data_types.py
@@ -0,0 +1,44 @@
+# Değişken ismi ve Değişkenin Değeri
+i = 10
+print(i)
+
+# pytonda C deki gibi önceden veri tipinin şeklini söylemeye gerek yoktur kendisi otomatik algılar...
+i = 3.4
+print(i)
+
+print(i * i * i)
+
+a = 4
+b = 3
+c = a + 2 * b
+print(c)
+
+
+# 1. Değişken isimleri bir sayı ile başlayamaz.
+# 2. Değişken ismi kelimelerden oluşuyorsa aralarında boşluk olamaz.
+# 3. :'",<>/?|\()!@#$%^&*~-+ Buradaki semboller değişken ismi içinde kullanılamaz.(Sadece _ sembolü kullanılabilir)
+# 4. Pythonda tanımlı anahtar kelimeler değişken ismi olarak kullanılamaz.(while, not vs. )
+
+#EXAMPLE
+pi_sayisi = 3.14
+cap = 4
+cevre = pi_sayisi * cap
+print(cevre)
+
+#İKİ SAYININ YERİNİ DEĞİŞTİRME
+a = 4
+b = 3
+print(a,b)
+
+a,b = b,a
+print(a,b)
+
+#SAYININ DEĞERİNİ ARTTIRMA
+a = 5
+print(a)
+
+a += 1
+print(a)
+
+#TEKLİ YORUM SATIRI
+"ÇOKLU YORUM SATIRI"
